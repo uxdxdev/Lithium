ba0dcc5506ece72265e7445718e224a4e3e46f60
David Morton <davidmortonmail@gmail.com>
2016-09-08 23:41:40 +0100

    Added new test

--- src/test/test_Server.cpp
+++ src/test/test_Server.cpp
@@ -3,11 +3,18 @@
 
 #include "server/Server.h"
 
-TEST(TestFunction, ReturnsXPlusOne) {
+TEST(TestFunction, ReturnsXPlusOne)
+{
     int result = Server::TestFunction(7);
     EXPECT_EQ(8, result);
 }
 
-TEST(TestFunction, PassOnNotEqual) {
+TEST(TestFunction, PassOnNotEqual)
+{
     EXPECT_NE(6, 7);
+}
+
+TEST(TestFunction, PassOnEqual)
+{
+    EXPECT_EQ(5, 5);
 }
